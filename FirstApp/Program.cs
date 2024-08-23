4e4208f1604a834ee89fb5614107d951a567c10e
GMNDS <gabriel@gmnds.com>
2024-08-23 16:47:07 -0300

    Primeiro programa com o C#

---
+++ FirstApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FirstApp
+{
+    enum ProductCategory
+    {
+        Electronics,
+        Clothing,
+        Food
+    }
+    struct Product 
+    {
+        public string Name;
+        public decimal Price;
+        // public string Category;
+        public ProductCategory Category;
+
+        public Product(string name, decimal price, ProductCategory category) 
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public decimal ProductInDolar(double rate) 
+        {
+            Price = Price * (decimal)rate;
+            return Price;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("Laptop", 1000, ProductCategory.Electronics);
+            System.Console.WriteLine(p.Name);
+            System.Console.WriteLine(p.Price);
+            System.Console.WriteLine(p.Category);
+
+            var pInDolar = p.ProductInDolar(5.5849);
+            System.Console.WriteLine(pInDolar);
+        }
+
+    }
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Calculadoa
+{
+    class Program 
+    {
+        static void Main(string[] args) {
+            Menu();
+        }
+
+        static void Menu() {
+            System.Console.WriteLine(@"
+            ╔══════════════════════════════════════════════════════════════════════════════╗
+            ║                          Escolha uma das opções abaixo                       ║
+            ║------------------------------------------------------------------------------║
+            ║                              [1] Soma ➕                                     ║
+            ║                              [2] Subtração ➖                                ║
+            ║                              [3] Divisão ➗                                  ║
+            ║                              [4] Multiplicação ✖️                             ║
+            ╚══════════════════════════════════════════════════════════════════════════════╝
+            ");
+
+            short.TryParse(Console.ReadLine(), out short input);
+
+            switch(input) {
+                case 1: ReadNums(Sum); break;
+                case 2: ReadNums(Subtraction); break;
+                case 3: ReadNums(Division); break;
+                case 4: ReadNums(Multiplication); break;
+                default: {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine(@"                   
+                    ████████████████████VOCÊ PRECISA DIGITAR UM NÚMERO VÁLIDO████████████████████");
+                    Console.ResetColor();
+                    Menu();
+                    };break;
+            }
+        }
+        static void ReadNums(Func<double, double, double> operation) {
+            while(true){
+                System.Console.WriteLine("Insira o primeiro valor");
+                string inputOne = Console.ReadLine();
+                System.Console.WriteLine("Insira o segundo valor");
+                string inputTwo = Console.ReadLine();
+
+                if (double.TryParse(inputOne, out double valueOne) && double.TryParse(inputTwo, out double valueTwo)) {
+                    double op = operation(valueOne, valueTwo);
+                                        Console.ForegroundColor = ConsoleColor.Blue;
+                    System.Console.WriteLine($@"                   
+                    ████████████████████O resultado da operação é {op}████████████████████");
+                    Console.ResetColor();
+                    Menu();
+                } else {
+                    System.Console.WriteLine("Insira valores válidos válido");
+                }
+            }
+        }  
+        
+        static double Sum(double ValueOne, double ValueTwo) {
+            double result = ValueOne + ValueTwo;
+            return result; 
+        }
+        static double Subtraction(double ValueOne, double ValueTwo) {
+            double result = ValueOne - ValueTwo;
+            return result; 
+        }
+        static double Division(double ValueOne, double ValueTwo) {
+            double result = ValueOne / ValueTwo;
+            return result; 
+        }
+        static double Multiplication(double ValueOne, double ValueTwo) {
+            double result = ValueOne * ValueTwo;
+            return result; 
+        }
+    }
+    
+}
